8d879223938c42a6c47163952b9f60a7b6796f26
Rishi <khakurel.rishikesh1@gmail.com>
2024-02-03 01:21:08 +0545

    netlify bug fixed

--- src/component/product.js
+++ src/component/product.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import Photo from "../assets/main.png"
 import { FcRating } from "react-icons/fc";
